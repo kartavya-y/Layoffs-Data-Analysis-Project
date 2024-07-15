efd0e0b42b1fb77bf99997204cf435b689531a53
kartavya-y <158798612+kartavya-y@users.noreply.github.com>
2024-07-16 01:58:45 +0530

    EDA.sql

--- EDA.sql
+++ EDA.sql
@@ -1,20 +1,12 @@
 -- EDA
 
--- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
-
--- normally when you start the EDA process you have some idea of what you're looking for
-
--- with this info we are just going to look around and see what we find!
 
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
 
--- EASIER QUERIES
 
 SELECT MAX(total_laid_off)
 FROM world_layoffs.layoffs_staging2;
-
-
 
 
 
@@ -24,35 +16,19 @@ SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off IS NOT NULL;
 
--- Which companies had 1 which is basically 100 percent of they company laid off
+-- Which companies had 100 percent of their company laid off
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1;
 -- these are mostly startups it looks like who all went out of business during this time
 
--- if we order by funcs_raised_millions we can see how big some of these companies were
+-- if we order by funds_raised we can see how big some of these companies were
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
--- BritishVolt looks like an EV company, Quibi! I recognize that company - wow raised like 2 billion dollars and went under - ouch
+-- BritishVolt looks like an EV company
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- SOMEWHAT TOUGHER AND MOSTLY USING GROUP BY--------------------------------------------------------------------------------------------------
 
 -- Companies with the biggest single Layoff
 
@@ -60,7 +36,6 @@ SELECT company, total_laid_off
 FROM world_layoffs.layoffs_staging
 ORDER BY 2 DESC
 LIMIT 5;
--- now that's just on a single day
 
 -- Companies with the most Total Layoffs
 SELECT company, SUM(total_laid_off)
@@ -77,8 +52,6 @@ FROM world_layoffs.layoffs_staging2
 GROUP BY location
 ORDER BY 2 DESC
 LIMIT 10;
-
--- this it total in the past 3 years or in the dataset
 
 SELECT country, SUM(total_laid_off)
 FROM world_layoffs.layoffs_staging2
@@ -103,14 +76,6 @@ GROUP BY stage
 ORDER BY 2 DESC;
 
 
-
-
-
-
--- TOUGHER QUERIES------------------------------------------------------------------------------------------------------------------------------------
-
--- Earlier we looked at Companies with the most Layoffs. Now let's look at that per year. It's a little more difficult.
--- I want to look at 
 
 WITH Company_Year AS 
 (
